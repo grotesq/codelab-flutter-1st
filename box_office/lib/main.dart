27dcb65f8f4ff8c1c666417d10c4e6669e1e656b
Naram Kim a.k.a. Unknown <unknown@grotesq.com>
2020-10-24 16:23:31 +0900

    추가 : 영화인 조회 기능

--- box_office/lib/main.dart
+++ box_office/lib/main.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
+import 'dart:math';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:http/http.dart' as http;
@@ -37,10 +39,12 @@ class _MyHomePageState extends State<MyHomePage> {
   List list = [];
   DateFormat requestFormat = DateFormat('yyyyMMdd');
   DateFormat displayFormat = DateFormat('yyyy년 MM월 dd일');
+  bool loaded = false;
 
   void load(DateTime date) async {
     setState((){
       list = [];
+      loaded = false;
     });
     final String key = '0a7200568417cd813479269673833592';
     String url = 'https://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json';
@@ -50,6 +54,7 @@ class _MyHomePageState extends State<MyHomePage> {
     var data = jsonDecode(response.body);
     setState(() {
       list = data['boxOfficeResult']['dailyBoxOfficeList'];
+      loaded = true;
     });
   }
 
@@ -93,6 +98,11 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ]
           ),
+          ( loaded ? Container() : Container(
+            padding: EdgeInsets.all(96),
+            alignment: Alignment.center,
+            child: CircularProgressIndicator()
+          ) ),
           Flexible(child: (
             ListView.builder(
               itemBuilder: (BuildContext context, int index) {
@@ -100,6 +110,10 @@ class _MyHomePageState extends State<MyHomePage> {
                   padding: EdgeInsets.all(12),
                   child: GestureDetector(
                     onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => MovieDetail(list[index]['movieCd'])),
+                      );
                       print( 'movie code : ${list[index]['movieCd']}');
                     },
                     child: Text('${list[index]['rnum']}위 ${list[index]['movieNm']}'),
@@ -111,6 +125,220 @@ class _MyHomePageState extends State<MyHomePage> {
           ))
         ],
       ),
+    );
+  }
+}
+
+class MovieDetail extends StatefulWidget {
+  String code;
+  MovieDetail( String code ) {
+    this.code = code;
+  }
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MovieDetailState();
+  }
+}
+
+class MovieDetailState extends State<MovieDetail> {
+  var movie;
+
+  void load() async {
+    final String key = '0a7200568417cd813479269673833592';
+    String url = 'https://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json';
+    url += '?key=$key';
+    url += '&movieCd=${widget.code}';
+    var response = await http.get(url);
+    var data = jsonDecode(response.body);
+    print( 'data : $data');
+    setState(() {
+      movie = data;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    load();
+    print(widget.code);
+  }
+  @override
+  Widget build(BuildContext context) {
+    if( movie == null ) {
+      return Scaffold(
+        body: Center(
+          child: (
+            CircularProgressIndicator()
+          ),
+        )
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('영화 상세 정보'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('제목 : ${movie['movieInfoResult']['movieInfo']['movieNm']}'),
+          Text('상영시간 : ${movie['movieInfoResult']['movieInfo']['showTm']}분'),
+          Flexible( child: (
+            ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (BuildContext context, int index) {
+                if( index == 0 ) {
+                  return Text( '출연 :');
+                }
+                List actors = movie['movieInfoResult']['movieInfo']['actors'];
+                return (
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context)=>PeopleList(actors[index-1]['peopleNm']))
+                      );
+                    },
+                    child: Text('${actors[index-1]['peopleNm']}')
+                  )
+                );
+              },
+              separatorBuilder: (BuildContext context, int index) => Text(' '),
+              itemCount: min(movie['movieInfoResult']['movieInfo']['actors'].length, 7)
+            )
+          ))
+        ],
+      )
+    );
+  }
+}
+
+class PeopleList extends StatefulWidget {
+  String name;
+  PeopleList(String name) {
+    this.name = name;
+  }
+
+  @override
+  State<StatefulWidget> createState() {
+    return PeopleListState();
+  }
+}
+
+class PeopleListState extends State<PeopleList> {
+  List list = [];
+
+  void load() async {
+    final String key = '0a7200568417cd813479269673833592';
+    String url = 'https://www.kobis.or.kr/kobisopenapi/webservice/rest/people/searchPeopleList.json';
+    url += '?key=$key';
+    url += '&peopleNm=${widget.name}';
+    var response = await http.get(url);
+    var data = jsonDecode(response.body);
+
+    setState(() {
+      list = data['peopleListResult']['peopleList'];
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    load();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar( title: Text( '영화인 검색 결과' ), ),
+      body: ListView.builder(
+        itemBuilder: (BuildContext context, int index ) {
+          return (
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => PeopleDetail(list[index]['peopleCd']))
+                );
+              },
+              child: (
+                Padding(
+                  padding: EdgeInsets.all(12),
+                  child: (
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text( list[index]['peopleNm'] ),
+                        Text( '대표작 : ${list[index]['filmoNames']}' ),
+                      ],
+                    )
+                  )
+                )
+              )
+            )
+          );
+        },
+        itemCount: list.length,
+      )
+    );
+  }
+}
+
+class PeopleDetail extends StatefulWidget {
+  String code;
+
+  PeopleDetail(String code) {
+    this.code = code;
+  }
+
+  @override
+  State<StatefulWidget> createState() {
+    return PeopleDetailState();
+  }
+}
+
+class PeopleDetailState extends State<PeopleDetail> {
+  var people;
+
+  void load() async {
+    final String key = '0a7200568417cd813479269673833592';
+    String url = 'https://www.kobis.or.kr/kobisopenapi/webservice/rest/people/searchPeopleInfo.json';
+    url += '?key=$key';
+    url += '&peopleCd=${widget.code}';
+    var response = await http.get(url);
+    var data = jsonDecode(response.body);
+
+    setState(() {
+      people = data['peopleInfoResult']['peopleInfo'];
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    load();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if( people == null ) {
+      return Scaffold(
+        body: Center(
+          child: CircularProgressIndicator()
+        )
+      );
+    }
+    return Scaffold(
+      appBar: AppBar(title: Text('영화인 상세 조회')),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('이름 : ${people['peopleNm']}'),
+          Text('성별 : ${people['sex']}'),
+          Text('역할 : ${people['repRoleNm']}'),
+        ],
+      )
     );
   }
 }
